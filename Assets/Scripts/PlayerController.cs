0777f38575fe560e22771a90606310ec46c8497b
jamesmcgrath88 <jamesmcgrath88@gmail.com>
2025-02-11 06:30:26 +0000

    Added basic jumping.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,18 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction LeftAction;
     public InputAction RightAction;
-
+    public InputAction JumpAction;
+    private Rigidbody2D PlayerRigidBody2D;
+    private BoxCollider2D PlayerBoxCollider2D;
+    [SerializeField] private LayerMask PlatformsLayerMask;
     void Start()
     {
         LeftAction.Enable();
         RightAction.Enable();
+        JumpAction.Enable();
+
+        PlayerRigidBody2D = GetComponent<Rigidbody2D>();
+        PlayerBoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -32,11 +39,21 @@ public class PlayerController : MonoBehaviour
             horizontal = 1.0f;
             playerSpriteRenderer.flipX = true;
         }
-        Debug.Log(horizontal);
 
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 0.1f * vertical;
         transform.position = position;
+
+        if (JumpAction.IsPressed() && IsGrounded())
+        {
+            float jumpVelocity = 25.0f;
+            PlayerRigidBody2D.linearVelocity = Vector2.up * jumpVelocity;
+        }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(PlayerBoxCollider2D.bounds.center, PlayerBoxCollider2D.bounds.size, 0.0f, Vector2.down, 0.1f, PlatformsLayerMask);
+        return rayCastHit2D.collider != null;
     }
 }
